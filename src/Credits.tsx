1e81806f6dea56530fbd9adf631614a0889c03ec
Luca Tabone <taboneluca3@gmail.com>
2026-04-11 00:49:55 +0200

    chore: add credits

---
+++ src/Credits.tsx
@@ -0,0 +1,14 @@
+export const Credits = () => (
+  <div style={{
+    opacity:.9,
+    right: '10px',
+    bottom: '10px',
+    position: 'fixed',
+    color: '#000000',
+    borderRadius: '4px',
+    background: '#ffffff',
+    fontFamily: 'monospace',
+    padding: '2px 14px 5px',
+    boxShadow: '0px 0px 10px 10px rgba(0, 0, 0, .3)',
+  }}>Made with ❤️ by <a href="https://lucatabone.com" target="_blank">me</a> ^.^/</div>
+)
